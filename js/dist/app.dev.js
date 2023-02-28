8eecbb4297e69020d3f8e04aba0454b8d4a36499
John (Nelson) Eneojo <30912661+johnenes@users.noreply.github.com>
2023-02-27 18:03:17 -0800

    Add files via upload

---
+++ js/dist/app.dev.js
@@ -0,0 +1,120 @@
+"use strict";
+
+// var for ul
+var ul = document.querySelector('ul');
+var btn = document.getElementById('add-btn'); // =================================ADD ITEM TO TASK LIST============================
+
+btn.addEventListener('click', function (e) {
+  e.preventDefault(); // var input tdg
+
+  var addInput = document.getElementById('add-input'); //Avoid submitting empty label
+
+  if (addInput.value !== '') {
+    //create list tag element
+    var li = document.createElement('li'),
+        textParagraph = document.createElement('p'),
+        iconParagraph = document.createElement('p'),
+        firstIcon = document.createElement('i'),
+        secondIcon = document.createElement('i'),
+        input = document.createElement('input'); //styling the icon
+
+    firstIcon.style.padding = '10px 10px';
+    firstIcon.style.color = "red"; //adding css clsassName attribute
+
+    secondIcon.className = "fa-sharp fa-solid fa-pen-to-square";
+    firstIcon.className = "fa-solid fa-xmark";
+    input.className = '"edit-note"';
+    textParagraph.className = "text"; //setAttribute for addInput
+
+    input.setAttribute('type', 'text');
+    textParagraph.textContent = addInput.value; //assign the list element to its children
+
+    iconParagraph.appendChild(firstIcon);
+    iconParagraph.appendChild(secondIcon); //  secondIcon.appendChild(svgIcon);
+
+    li.appendChild(textParagraph);
+    li.appendChild(iconParagraph);
+    li.appendChild(input);
+    ul.appendChild(li); //clear text input clicking on add button
+
+    addInput.value = '';
+  }
+}); // =================================END OF ADDING ITEM TO TASK LIST=======================
+//===============================Edit and Delete===========================================
+
+ul.addEventListener('click', function (e) {
+  //to make sure it only execute once is click, nothing esle execute except the class containing 
+  //fa-pen-to-square 
+  if (e.target.classList[2] === 'fa-pen-to-square') {
+    //  console.log(e.target.classList[2]) // to access class attribute use the index . 
+    //we want to access the paragraph holding the two <i>
+    var parentParagraph = e.target.parentNode; //  we need the parentParagraph when click event occur
+
+    parentParagraph.style.display = 'none';
+    var note = parentParagraph.previousElementSibling;
+    var input = parentParagraph.nextElementSibling; // to appear input element same time of event 
+
+    input.style.display = 'block'; // display input text in block
+    //taking note for input
+
+    input.value = note.textContent; ///Event Listeners for handing input value using kepress event 
+
+    input.addEventListener('keypress', function (e) {
+      //
+      if (e.keyCode === 13) {
+        if (input.value !== '') {
+          // grab the input content and assign it note content 
+          note.textContent = input.value; // we need to access the icon delete
+
+          parentParagraph.style.display = 'block';
+          input.style.display = 'none'; //  weed to control the ul from submiting when blank
+        } else {
+          // remove the ul tag from the form
+          var li = input.parentNode;
+          console.log(li);
+          li.parentNode.removeChild(li);
+        }
+      }
+    });
+  } else if (e.target.classList[1] === "fa-xmark") {
+    var list = e.target.parentNode.parentNode;
+    list.parentNode.removeChild(list);
+  }
+}); // =================================END OF Edit and Delete=========================================== 
+// =================================Hide notes=======================================================
+
+var hideItem = document.querySelector('#hide');
+hideItem.addEventListener('click', function (e) {
+  //check conditon if the items is checked then return true
+  var label = document.querySelector('label');
+
+  if (hideItem.checked) {
+    label.textContent = "Unhide Notes";
+    ul.style.display = 'none';
+  } else {
+    label.textContent = "hide Notes";
+    ul.style.display = 'block';
+  }
+}); //==================================End of the Hide notes sections =======================================
+//==================================Search sections=======================================================
+
+var searchItems = document.querySelector('#search-list input');
+searchItems.addEventListener('keyup', function (e) {
+  //grabing the input from the search box  and convert toUpperCase
+  var searchChar = searchItems.value.toUpperCase(); //  console.log(searchChar)
+  //accessing the list items by tag name.
+
+  var notes = ul.getElementsByTagName('li'); //in order to compare the items we neeed to loop through the items.
+
+  Array.from(notes).forEach(function (note) {
+    // let grab the content of list items
+    var paragraphText = note.firstElementChild.textContent; // transform the paragraphText toUpperCase
+
+    if (paragraphText.toUpperCase().indexOf(searchChar) !== -1) {
+      note.style.display = 'block';
+    } else {
+      note.style.display = 'none';
+    }
+  });
+});
+//# sourceMappingURL=app.dev.js.map
---
+++ js/dist/jscript.dev.js
@@ -0,0 +1,84 @@
+"use strict";
+
+// //accessing the ul element
+var ul = document.querySelector('ul');
+var btn = document.getElementById('add-btn');
+btn.addEventListener('click', function (e) {
+  e.preventDefault(); //     //Accessing input from the form element add
+
+  var addInput = document.getElementById('add-input'); //     //check to make sure no empty input is submitted.
+
+  if (addInput.value !== '') {
+    // we need to create new element tag as li
+    var li = document.createElement('li'),
+        firstPar = document.createElement('p'),
+        secondPar = document.createElement('p'),
+        firstIcon = document.createElement('i'),
+        secondIcon = document.createElement('i'),
+        input = document.createElement('input'); //styling the icon
+
+    firstIcon.style.padding = '10px 10px';
+    firstIcon.style.color = "red"; //assign each element the class attributes to the new created element
+
+    secondIcon.className = "fa-sharp fa-solid fa-pen-to-square";
+    firstIcon.className = "fa-solid fa-xmark";
+    input.className = '"edit-note"';
+    firstPar.className = "text"; //setAttribute for addInput
+
+    input.setAttribute('type', 'text');
+    firstPar.textContent = addInput.value; //Now let assign the each newest tag element to their parent element
+
+    secondPar.appendChild(firstIcon);
+    secondPar.appendChild(secondIcon);
+    li.appendChild(firstPar);
+    li.appendChild(secondPar);
+    li.appendChild(input);
+    ul.appendChild(li); //make sure not text should appear on the input text after it been added 
+
+    addInput.value = '';
+  }
+}); // // ========================Working on Edit notes==================================
+// //using the ul in the gobal execution context
+
+ul.addEventListener('click', function (e) {
+  //accessing the delete obj in the class using class attribute and make sure that 
+  //it only click that particular object
+  if (e.target.classList[2] === 'fa-pen-to-square') {
+    // need to access the parent tag hold fa-pen-to-square tag  
+    var parText = e.target.parentNode;
+    parText.style.display = 'none'; // accessing the tag before this current tag also after the current tage
+
+    var note = parText.previousElementSibling;
+    var input = parText.nextElementSibling; //grab the text in the input and sign to note
+
+    input.style.display = 'block';
+    input.value = note.textContent; // THE TEXT IN INPUT CAN NOW BE SUBMITED TO THE NOTES ONCE KEYPRESS IS RELESASED
+
+    input.addEventListener('keypress', function (e) {
+      if (e.keyCode === 13) {
+        if (input.value !== '') {
+          //assign the note to the input element
+          note.textContent = input.value;
+          console.log(note.textContent);
+          input.style.display = 'block';
+        } else {
+          var li = input.parentNode;
+          li.parentNode.removeChild(li);
+        }
+      }
+    });
+  } else if (e.target.classList[1] === 'fa-xmark') {
+    // console.log('Please select')
+    //remove the list tag once keyboard enter is being pressed
+    var list = e.target.parentNode.parentNode;
+    list.parentNode.removeChild(list);
+  }
+}); //===============Hide Notes=================================
+
+var hideNotes = document.querySelector('#hide-list input');
+hideNotes.addEventListener('click', function (e) {
+  if (hideNotes.checked) {
+    ul.style.display = 'none';
+  }
+});
+//# sourceMappingURL=jscript.dev.js.map
